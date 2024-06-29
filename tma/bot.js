a9adced1731ad57f5fa56327e6367af6ec8e01e1
manas <tmanas2004@gmail.com>
2024-06-29 18:50:31 +0530

    upf

--- tma/bot.js
+++ tma/bot.js
@@ -8,4 +8,19 @@ bot.on('message', (msg) => {
     if (messageText === '/start') {
       bot.sendMessage(chatId, 'Welcome to the bot!');
     }
+    else if(messageText === '/help'){
+        bot.sendMessage(chatId, 'Here are the available commands:\n\n' +
+        "/start - Begin interacting with the bot\n" +
+        "/help - Display available commands\n" +
+        "/open - Open the Crypto Critters Trading App");
+    }
+    else if(messageText === '/open'){
+        bot.sendMessage(chatId, 'Opening Crypto Critters Trading App...');
+        const appLink = "https://your-react-app-url.com";
+        const appMessage = `Click here to open the app: ${appLink}`;
+        bot.sendMessage(chatId, appMessage);
+    }
+    else{
+        bot.sendMessage(chatId, 'Invalid command. Type /help for a list of available commands.');
+    }
   });
